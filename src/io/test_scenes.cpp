4422dc7040328b8277f94171dfa739591616442d
Artem Kharytoniuk <artemalive@gmail.com>
2019-01-12 12:21:49 +0100

    Updated test scene.

--- src/io/test_scenes.cpp
+++ src/io/test_scenes.cpp
@@ -16,11 +16,19 @@ Scene_Data load_conference_scene() {
     XYZ xyz = s.emission_spectrum_to_XYZ();
 
     RGB_Point_Light_Data light;
-    light.position = Vector3(0, 0, 1.5);
+    light.position = Vector3(2, 0, 1.5);
     light.intensity = RGB(xyz);
 
+    RGB_Point_Light_Data light2;
+    light2.position = Vector3(-1, -1, 1.0);
+    light2.intensity = RGB(xyz);
+
+    Mesh_Load_Params mesh_load_params;
+    mesh_load_params.transform = uniform_scale(to_world_axis_orientation, 0.003f);
+
     Scene_Data scene;
-    scene.meshes = load_obj("conference/conference.obj", uniform_scale(to_world_axis_orientation, 0.003f));
+    scene.meshes = load_obj("conference/conference.obj", mesh_load_params);
     scene.rgb_point_lights.push_back(light);
+    scene.rgb_point_lights.push_back(light2);
     return scene;
 }
