5c50d2a505b17519e95c185dc18f6299a3b933ed
Artem Kharytoniuk <artemalive@gmail.com>
2020-06-27 18:16:18 +0200

    Add profiling helpers

--- src/lib/common.h
+++ src/lib/common.h
@@ -2,6 +2,8 @@
 
 #define ASSERT(expression) if (!(expression)) __debugbreak()
 
+#define ENABLE_PROFILING 1
+
 constexpr float Pi = 3.14159265f;
 constexpr float Pi2 = 6.2831853f;
 constexpr float Pi_Over_2 = 1.57079632f;
@@ -39,7 +41,7 @@ int64_t elapsed_milliseconds(Timestamp timestamp);
 int64_t elapsed_microseconds(Timestamp timestamp);
 int64_t elapsed_nanoseconds(Timestamp timestamp);
 
-#if 1
+#if ENABLE_PROFILING
 #define START_TIMER { Timestamp t;
 #define STOP_TIMER(message) \
 	auto d = elapsed_nanoseconds(t); \
@@ -48,9 +50,25 @@ int64_t elapsed_nanoseconds(Timestamp timestamp);
 		t0 = Timestamp(); \
 		printf(message ## " time = %lld  microseconds\n", d / 1000); } }
 
+struct Profile_Scope {
+    const char* message = nullptr;
+    Timestamp t;
+
+    Profile_Scope(const char* message)
+        : message(message) {}
+
+    ~Profile_Scope() {
+        printf("Profiler: %s %.2f ms\n", message, elapsed_microseconds(t) / 1000.f);
+    }
+};
+
+#define REPORT_FUNCTION_TIME() Profile_Scope function_time_reporter(__FUNCTION__);
+#define REPORT_SCOPE_TIME(message) Profile_Scope function_time_reporter(message);
+
 #else
 #define START_TIMER
 #define STOP_TIMER(...)
+REPORT_FUNCTION_TIME()
 #endif
 
 // http://www.reedbeta.com/blog/python-like-enumerate-in-cpp17/
