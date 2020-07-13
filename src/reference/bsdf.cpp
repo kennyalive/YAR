106911cb1828287380b3ae21cf204ee6fde27ce7
Artem Kharytoniuk <artemalive@gmail.com>
2020-07-13 19:01:51 +0200

    ref: Extended BSDF interface to support MIS

--- src/reference/bsdf.cpp
+++ src/reference/bsdf.cpp
@@ -4,28 +4,29 @@
 
 #include "context.h"
 #include "parameter_evaluation.h"
+#include "sampling.h"
 #include "scattering.h"
 #include "shading_context.h"
 
-const BSDF* create_bsdf(const Render_Context& global_ctx, Thread_Context& thread_ctx, const Shading_Context& shading_ctx, Material_Handle material) {
+const BSDF* create_bsdf(const Scene_Context& scene_ctx, Thread_Context& thread_ctx, const Shading_Context& shading_ctx, Material_Handle material) {
     switch (material.type) {
         case Material_Type::lambertian:
         {
-            const Lambertian_Material& params = global_ctx.materials.lambertian[material.index];
+            const Lambertian_Material& params = scene_ctx.materials.lambertian[material.index];
             void* bsdf_allocation = thread_ctx.memory_pool.allocate<Lambertian_BRDF>();
-            return new (bsdf_allocation) Lambertian_BRDF(global_ctx, shading_ctx, params);
+            return new (bsdf_allocation) Lambertian_BRDF(scene_ctx, shading_ctx, params);
         }
         case Material_Type::metal:
         {
-            const Metal_Material& params = global_ctx.materials.metal[material.index];
+            const Metal_Material& params = scene_ctx.materials.metal[material.index];
             void* bsdf_allocation = thread_ctx.memory_pool.allocate<Metal_BRDF>();
-            return new (bsdf_allocation) Metal_BRDF(global_ctx, shading_ctx, params);
+            return new (bsdf_allocation) Metal_BRDF(scene_ctx, shading_ctx, params);
         }
         case Material_Type::plastic:
         {
-            const Plastic_Material& params = global_ctx.materials.plastic[material.index];
+            const Plastic_Material& params = scene_ctx.materials.plastic[material.index];
             void* bsdf_allocation = thread_ctx.memory_pool.allocate<Plastic_BRDF>();
-            return new (bsdf_allocation) Plastic_BRDF(global_ctx, shading_ctx, params);
+            return new (bsdf_allocation) Plastic_BRDF(scene_ctx, shading_ctx, params);
         }
         default:
         {
@@ -35,23 +36,51 @@ const BSDF* create_bsdf(const Render_Context& global_ctx, Thread_Context& thread
     }
 }
 
-Lambertian_BRDF::Lambertian_BRDF(const Render_Context& global_ctx, const Shading_Context& shading_ctx, const Lambertian_Material& material)
-    : reflectance(evaluate_rgb_parameter(global_ctx, shading_ctx, material.reflectance))
-{}
+BSDF::BSDF(const Shading_Context& shading_ctx)
+    : shading_ctx(&shading_ctx)
+    , N(shading_ctx.N)
+{
+}
 
-ColorRGB Lambertian_BRDF::evaluate(const Vector3&, const Vector3&) const {
+//
+// Lambertian BRDF
+//
+Lambertian_BRDF::Lambertian_BRDF(const Scene_Context& scene_ctx, const Shading_Context& shading_ctx, const Lambertian_Material& material)
+    : BSDF(shading_ctx)
+{
+    reflection_scattering = true;
+    reflectance = evaluate_rgb_parameter(scene_ctx, shading_ctx, material.reflectance);
+}
+
+ColorRGB Lambertian_BRDF::evaluate(const Vector3& /*wo*/, const Vector3& /*wi*/) const {
     return Pi_Inv * reflectance;
 }
 
-Metal_BRDF::Metal_BRDF(const Render_Context& global_ctx, const Shading_Context& shading_ctx, const Metal_Material& material)
+ColorRGB Lambertian_BRDF::sample(Vector2 u, const Vector3& wo, Vector3* wi, float* pdf) const {
+    Vector3 local_dir = sample_hemisphere_cosine(u);
+    *wi = shading_ctx->local_to_world(local_dir);
+    *pdf = Lambertian_BRDF::pdf(wo, *wi);
+    return Pi_Inv * reflectance;
+}
+
+float Lambertian_BRDF::pdf(const Vector3& /*wo*/, const Vector3& wi) const {
+    ASSERT(dot(N, wi) >= 0.f);
+    return dot(N, wi) / Pi; // pdf for cosine-weighted hemisphere sampling
+}
+
+//
+// Metal BRDF
+//
+Metal_BRDF::Metal_BRDF(const Scene_Context& scene_ctx, const Shading_Context& shading_ctx, const Metal_Material& material)
+    : BSDF(shading_ctx)
 {
-    scene_ctx = &global_ctx;
+    this->scene_ctx = &scene_ctx;
     this->shading_ctx = &shading_ctx;
 
-    roughness = evaluate_float_parameter(global_ctx, shading_ctx, material.roughness);
-    eta_i = evaluate_float_parameter(global_ctx, shading_ctx, material.eta_i);
-    eta_t = evaluate_rgb_parameter(global_ctx, shading_ctx, material.eta);
-    k_t = evaluate_rgb_parameter(global_ctx, shading_ctx, material.k);
+    roughness = evaluate_float_parameter(scene_ctx, shading_ctx, material.roughness);
+    eta_i = evaluate_float_parameter(scene_ctx, shading_ctx, material.eta_i);
+    eta_t = evaluate_rgb_parameter(scene_ctx, shading_ctx, material.eta);
+    k_t = evaluate_rgb_parameter(scene_ctx, shading_ctx, material.k);
 }
 
 ColorRGB Metal_BRDF::evaluate(const Vector3& wo, const Vector3& wi) const {
@@ -70,14 +99,18 @@ ColorRGB Metal_BRDF::evaluate(const Vector3& wo, const Vector3& wi) const {
     return f;
 }
 
-Plastic_BRDF::Plastic_BRDF(const Render_Context& global_ctx, const Shading_Context& shading_ctx, const Plastic_Material& params)
+//
+// Plastic BRDF
+//
+Plastic_BRDF::Plastic_BRDF(const Scene_Context& scene_ctx, const Shading_Context& shading_ctx, const Plastic_Material& params)
+    : BSDF(shading_ctx)
 {
-    scene_ctx = &global_ctx;
+    this->scene_ctx = &scene_ctx;
     this->shading_ctx = &shading_ctx;
 
-    roughness = evaluate_float_parameter(global_ctx, shading_ctx, params.roughness);
-    r0 = evaluate_float_parameter(global_ctx, shading_ctx, params.r0);
-    diffuse_reflectance = evaluate_rgb_parameter(global_ctx, shading_ctx, params.diffuse_reflectance);
+    roughness = evaluate_float_parameter(scene_ctx, shading_ctx, params.roughness);
+    r0 = evaluate_float_parameter(scene_ctx, shading_ctx, params.r0);
+    diffuse_reflectance = evaluate_rgb_parameter(scene_ctx, shading_ctx, params.diffuse_reflectance);
 }
 
 ColorRGB Plastic_BRDF::evaluate(const Vector3& wo, const Vector3& wi) const {
--- src/reference/bsdf.h
+++ src/reference/bsdf.h
@@ -1,22 +1,36 @@
 #pragma once
 
 #include "lib/material.h"
+#include "lib/vector.h"
 
-struct Render_Context;
+struct Scene_Context;
 struct Shading_Context;
 struct Thread_Context;
 
 struct BSDF {
     virtual ColorRGB evaluate(const Vector3& wo, const Vector3& wi) const = 0;
+    virtual ColorRGB sample(Vector2 u, const Vector3& wo, Vector3* wi, float* pdf) const = 0;
+    virtual float pdf(const Vector3& wo, const Vector3& wi) const = 0;
+
+    BSDF(const Shading_Context& shading_ctx);
+
+    const Shading_Context* shading_ctx;
+    Vector3 N; // shading normal (extracted from shading_ctx)
+
+    // types of scattering modeled by this bsdf
+    bool reflection_scattering = false;
+    bool transmission_scattering = false;
 };
 
-const BSDF* create_bsdf(const Render_Context& global_ctx, Thread_Context& thread_ctx, const Shading_Context& shading_ctx, Material_Handle material);
+const BSDF* create_bsdf(const Scene_Context& scene_ctx, Thread_Context& thread_ctx, const Shading_Context& shading_ctx, Material_Handle material);
 
 struct Lambertian_BRDF : public BSDF {
     ColorRGB reflectance;
 
-    Lambertian_BRDF(const Render_Context& global_ctx, const Shading_Context& shading_ctx, const Lambertian_Material& material);
+    Lambertian_BRDF(const Scene_Context& scene_ctx, const Shading_Context& shading_ctx, const Lambertian_Material& material);
     ColorRGB evaluate(const Vector3& wo, const Vector3& wi) const override;
+    ColorRGB sample(Vector2 u, const Vector3& wo, Vector3* wi, float* pdf) const override;
+    float pdf(const Vector3& wo, const Vector3& wi) const override;
 };
 
 struct Metal_BRDF : public BSDF {
@@ -25,11 +39,13 @@ struct Metal_BRDF : public BSDF {
     ColorRGB eta_t;
     ColorRGB k_t;
 
-    const Render_Context* scene_ctx;
+    const Scene_Context* scene_ctx;
     const Shading_Context* shading_ctx;
 
-    Metal_BRDF(const Render_Context& global_ctx, const Shading_Context& shading_ctx, const Metal_Material& material);
+    Metal_BRDF(const Scene_Context& scene_ctx, const Shading_Context& shading_ctx, const Metal_Material& material);
     ColorRGB evaluate(const Vector3& wo, const Vector3& wi) const override;
+    ColorRGB sample(Vector2 u, const Vector3& wo, Vector3* wi, float* pdf) const override { ASSERT(false); return ColorRGB{}; }
+    float pdf(const Vector3& wo, const Vector3& wi) const override { ASSERT(false); return 0.f; }
 };
 
 struct Plastic_BRDF : public BSDF {
@@ -37,9 +53,11 @@ struct Plastic_BRDF : public BSDF {
     float r0;
     ColorRGB diffuse_reflectance;
 
-    const Render_Context* scene_ctx;
+    const Scene_Context* scene_ctx;
     const Shading_Context* shading_ctx;
 
-    Plastic_BRDF(const Render_Context& global_ctx, const Shading_Context& shading_ctx, const Plastic_Material& params);
+    Plastic_BRDF(const Scene_Context& scene_ctx, const Shading_Context& shading_ctx, const Plastic_Material& params);
     ColorRGB evaluate(const Vector3& wo, const Vector3& wi) const override;
+    ColorRGB sample(Vector2 u, const Vector3& wo, Vector3* wi, float* pdf) const override { ASSERT(false); return ColorRGB{}; }
+    float pdf(const Vector3& wo, const Vector3& wi) const override { ASSERT(false); return 0.f; }
 };
