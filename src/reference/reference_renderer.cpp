59c9d9b5c5249e12f8dd1ce167049fa6843cf9c6
Artem Kharytoniuk <artemalive@gmail.com>
2021-06-20 12:17:33 +0200

    Reset dielectric state for each sample
    There is no guarantee that when we finished with current sample
    the dielectric state is set to default value again. For example,
    this happens if we have a diffuse surface hit that was put
    inside a dielectric. Another scenerio is debug configuration
    where dielectric volume is not closed intentionally.

--- src/reference/reference_renderer.cpp
+++ src/reference/reference_renderer.cpp
@@ -167,7 +167,6 @@ static void render_tile(const Scene_Context& scene_ctx, Thread_Context& thread_c
     for (int y = sample_bounds.p0.y; y < sample_bounds.p1.y; y++) {
         for (int x = sample_bounds.p0.x; x < sample_bounds.p1.x; x++) {
             thread_ctx.pixel_sampler.next_pixel();
-            thread_ctx.current_dielectric_material = Null_Material;
 
             // variance estimation
             double luminance_sum = 0.0;
@@ -175,6 +174,7 @@ static void render_tile(const Scene_Context& scene_ctx, Thread_Context& thread_c
 
             do {
                 thread_ctx.memory_pool.reset();
+                thread_ctx.current_dielectric_material = Null_Material;
 
                 Vector2 film_pos = Vector2((float)x, (float)y) + thread_ctx.pixel_sampler.get_image_plane_sample();
 
