568ad8224a2c3574f169041f1d3016484c744297
Artem Kharytoniuk <artemalive@gmail.com>
2022-04-26 21:22:44 +0200

    ref: Refactor path tracing algorithm

--- src/reference/path_tracing.cpp
+++ src/reference/path_tracing.cpp
@@ -21,44 +21,31 @@ ColorRGB estimate_path_contribution(Thread_Context& thread_ctx, const Ray& ray, 
     ColorRGB path_coeff = Color_White;
     Ray current_ray = ray;
 
-    bool sample_emitted_radiance_after_delta_layer = false;
-
     ColorRGB L;
     while (true) {
+        bool sample_emitted_radiance_after_delta_layer = shading_ctx.specular_scattering.sample_delta_direction;
+
         const Differential_Rays* p_differential_rays = (path_ctx.bounce_count == 0) ? &differential_rays : nullptr;
         bool hit_found = trace_ray(thread_ctx, current_ray, p_differential_rays);
 
-        if (sample_emitted_radiance_after_delta_layer) {
-            sample_emitted_radiance_after_delta_layer = false;
-            if (hit_found)
-                L += path_coeff * get_emitted_radiance(thread_ctx);
-            else if (scene_ctx.has_environment_light_sampler)
-                L += path_coeff * scene_ctx.environment_light_sampler.get_filtered_radiance_for_direction(shading_ctx.miss_ray.direction);
-        }
+        bool specular_bounce = shading_ctx.specular_scattering.type != Specular_Scattering_Type::none;
 
-        // If we hit perfect specular surface then keep bouncing until we reach finite bsdf or exit the scene.
-        if (shading_ctx.specular_scattering.type != Specular_Scattering_Type::none) {
+        if (specular_bounce) {
             ColorRGB specular_attenuation = Color_White;
             hit_found = trace_specular_bounces(thread_ctx, max_bounces, &specular_attenuation);
             path_coeff *= specular_attenuation;
+        }
 
+        if (path_ctx.bounce_count == 0 || sample_emitted_radiance_after_delta_layer || specular_bounce) {
             if (hit_found)
                 L += path_coeff * get_emitted_radiance(thread_ctx);
             else if (scene_ctx.has_environment_light_sampler)
                 L += path_coeff * scene_ctx.environment_light_sampler.get_filtered_radiance_for_direction(shading_ctx.miss_ray.direction);
-
-            // check if we reached max bounce limit due to bounces on specular surfaces
-            if (path_ctx.bounce_count == max_bounces)
-                break;
         }
 
-        // Collect directly visible emitted light.
-        if (path_ctx.bounce_count == 0) {
-            if (hit_found)
-                L += get_emitted_radiance(thread_ctx);
-            else if (scene_ctx.has_environment_light_sampler)
-                L += scene_ctx.environment_light_sampler.get_filtered_radiance_for_direction(shading_ctx.miss_ray.direction);
-        }
+        // check if we reached max bounce limit due to bounces on specular surfaces
+        if (specular_bounce && path_ctx.bounce_count == max_bounces)
+            break;
 
         // About area light check: in current design we don't have scattering on area light
         // sources (shading_ctx.bsdf == nullptr). Hitting area light ends path generation
@@ -67,11 +54,12 @@ ColorRGB estimate_path_contribution(Thread_Context& thread_ctx, const Ray& ray, 
             break;
 
         float u_light_index = thread_ctx.pixel_sampler.get_next_1d_sample();
-        Vector2 u_light = thread_ctx.pixel_sampler.get_next_2d_sample();
-        Vector2 u_bsdf = thread_ctx.pixel_sampler.get_next_2d_sample();
+        Vector2 u_light_mis = thread_ctx.pixel_sampler.get_next_2d_sample();
+        Vector2 u_bsdf_mis = thread_ctx.pixel_sampler.get_next_2d_sample();
+        Vector2 u_bsdf_next_segment = thread_ctx.pixel_sampler.get_next_2d_sample();
 
         // Sample light and add its contribution to current path.
-        ColorRGB direct_lighting = estimate_direct_lighting_from_single_sample(thread_ctx, u_light_index, u_light, u_bsdf);
+        ColorRGB direct_lighting = estimate_direct_lighting_from_single_sample(thread_ctx, u_light_index, u_light_mis, u_bsdf_mis);
         L += path_coeff * direct_lighting;
 
         if (++path_ctx.bounce_count == max_bounces)
@@ -79,22 +67,17 @@ ColorRGB estimate_path_contribution(Thread_Context& thread_ctx, const Ray& ray, 
 
         // Generate next path segment.
         Vector3 wi;
-        float bsdf_pdf;
-
-        Vector2 u = thread_ctx.pixel_sampler.get_next_2d_sample();
-
         if (shading_ctx.specular_scattering.sample_delta_direction) {
             wi = shading_ctx.specular_scattering.delta_direction;
             path_coeff *= shading_ctx.specular_scattering.scattering_coeff;
-            sample_emitted_radiance_after_delta_layer = true;
         }
         else {
-            ColorRGB f = shading_ctx.bsdf->sample(u, shading_ctx.wo, &wi, &bsdf_pdf);
+            float bsdf_pdf;
+            ColorRGB f = shading_ctx.bsdf->sample(u_bsdf_next_segment, shading_ctx.wo, &wi, &bsdf_pdf);
             if (f.is_black())
                 break;
             path_coeff *= f * (shading_ctx.specular_scattering.finite_scattering_weight * std::abs(dot(shading_ctx.normal, wi)) / bsdf_pdf);
         }
-
         current_ray.origin = shading_ctx.get_ray_origin_using_control_direction(wi);
         current_ray.direction = wi;
 
