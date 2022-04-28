1051cc21c695b1bb16d51be0a3ef0b11f674db71
Artem Kharytoniuk <artemalive@gmail.com>
2022-04-28 17:46:28 +0200

    ref: Replace trace_specular_bounces with corresponding code
    Currently it looks it's a better decision both from total amount of complexity
    and also from correctness point of view. tracer_specular_bounces does not stop
    unless it reaches non-delta surface. It means we can not collect emitted light
    from delta surfaces. It is also prevented application of russian rouletee during
    traversal through delta surfaces.

--- src/lib/raytracer_config.h
+++ src/lib/raytracer_config.h
@@ -20,6 +20,12 @@ struct Raytracer_Config {
     // 3 - second bound of indirect lighting
     int max_light_bounces = 32;
 
+    // Do not apply russian roulette if any radiance component is greater-equal than this value.
+    float russian_roulette_radiance_threshold = 1.f;
+
+    // Do not apply russian roulette until we have this number of light bounces.
+    int russian_roulette_bounce_count_threshold = 3;
+
     // This constant defines when to stop computing differential rays when bouncing off
     // perfect specular surfaces multiple times. Differential rays approximation gets
     // increasingly less precise with each bounce and at some point it ceases to be
--- src/reference/path_tracing.cpp
+++ src/reference/path_tracing.cpp
@@ -9,43 +9,51 @@
 
 #include "lib/scene.h"
 
-constexpr int bounce_count_when_to_apply_russian_roulette = 3;
-
-ColorRGB estimate_path_contribution(Thread_Context& thread_ctx, const Ray& ray, const Differential_Rays& differential_rays) {
+ColorRGB estimate_path_contribution(Thread_Context& thread_ctx, const Ray& ray,
+    const Differential_Rays& differential_rays)
+{
     const Scene_Context& scene_ctx = *thread_ctx.scene_context;
     const Shading_Context& shading_ctx = thread_ctx.shading_context;
     Path_Context& path_ctx = thread_ctx.path_context;
+    const Raytracer_Config& rt_config = scene_ctx.raytracer_config;
 
-    const int max_bounces = scene_ctx.raytracer_config.max_light_bounces;
+    const int max_bounces = rt_config.max_light_bounces;
+    const int max_differential_ray_bounces = rt_config.max_differential_ray_specular_bounces;
 
-    ColorRGB path_coeff = Color_White;
     Ray current_ray = ray;
+    ColorRGB path_coeff = Color_White;
+
+    // These differential rays are computed after scattering on delta surfaces.
+    Differential_Rays specular_differential_rays;
+    bool use_specular_differential_rays = false;
 
     ColorRGB L;
     while (true) {
-        bool sample_emitted_radiance_after_delta_layer = shading_ctx.specular_scattering.sample_delta_direction;
+        const bool sample_emitted_radiance_after_delta_layer =
+            shading_ctx.specular_scattering.sample_delta_direction ||
+            shading_ctx.specular_scattering.type != Specular_Scattering_Type::none;
 
-        const Differential_Rays* p_differential_rays = (path_ctx.bounce_count == 0) ? &differential_rays : nullptr;
-        bool hit_found = trace_ray(thread_ctx, current_ray, p_differential_rays);
-
-        bool specular_bounce = shading_ctx.specular_scattering.type != Specular_Scattering_Type::none;
-
-        if (specular_bounce) {
-            ColorRGB specular_attenuation = Color_White;
-            hit_found = trace_specular_bounces(thread_ctx, max_bounces, &specular_attenuation);
-            path_coeff *= specular_attenuation;
+        const Differential_Rays* p_differential_rays = nullptr;
+        if (path_ctx.bounce_count == 0) {
+            p_differential_rays = &differential_rays;
+        }
+        else if (use_specular_differential_rays) {
+            p_differential_rays = &specular_differential_rays;
+            use_specular_differential_rays = false;
         }
 
-        if (path_ctx.bounce_count == 0 || sample_emitted_radiance_after_delta_layer || specular_bounce) {
+        bool hit_found = trace_ray(thread_ctx, current_ray, p_differential_rays);
+
+        // Collect directly visible and delta scattered emitted light.
+        if (path_ctx.bounce_count == 0 || sample_emitted_radiance_after_delta_layer) {
             if (hit_found)
                 L += path_coeff * get_emitted_radiance(thread_ctx);
             else if (scene_ctx.has_environment_light_sampler)
                 L += path_coeff * scene_ctx.environment_light_sampler.get_filtered_radiance_for_direction(shading_ctx.miss_ray.direction);
-        }
 
-        // check if we reached max bounce limit due to bounces on specular surfaces
-        if (specular_bounce && path_ctx.bounce_count == max_bounces)
-            break;
+            if (path_ctx.bounce_count == max_bounces)
+                break;
+        }
 
         // About area light check: in current design we don't have scattering on area light
         // sources (shading_ctx.bsdf == nullptr). Hitting area light ends path generation
@@ -58,42 +66,75 @@ ColorRGB estimate_path_contribution(Thread_Context& thread_ctx, const Ray& ray, 
         Vector2 u_bsdf_mis = thread_ctx.pixel_sampler.get_next_2d_sample();
         Vector2 u_bsdf_next_segment = thread_ctx.pixel_sampler.get_next_2d_sample();
 
-        // Sample light and add its contribution to current path.
-        ColorRGB direct_lighting = estimate_direct_lighting_from_single_sample(thread_ctx, u_light_index, u_light_mis, u_bsdf_mis);
-        L += path_coeff * direct_lighting;
+        if (shading_ctx.specular_scattering.type == Specular_Scattering_Type::none) {
+            // Sample light and add contribution from the current path.
+            ColorRGB direct_lighting = estimate_direct_lighting_from_single_sample(thread_ctx, u_light_index, u_light_mis, u_bsdf_mis);
+            L += path_coeff * direct_lighting;
 
-        if (++path_ctx.bounce_count == max_bounces)
-            break;
-
-        // Generate next path segment.
-        Vector3 wi;
-        if (shading_ctx.specular_scattering.sample_delta_direction) {
-            wi = shading_ctx.specular_scattering.delta_direction;
-            path_coeff *= shading_ctx.specular_scattering.scattering_coeff;
-        }
-        else {
-            float bsdf_pdf;
-            ColorRGB f = shading_ctx.bsdf->sample(u_bsdf_next_segment, shading_ctx.wo, &wi, &bsdf_pdf);
-            if (f.is_black())
+            if (++path_ctx.bounce_count == max_bounces)
                 break;
-            path_coeff *= f * (shading_ctx.specular_scattering.finite_scattering_weight * std::abs(dot(shading_ctx.normal, wi)) / bsdf_pdf);
+
+            // Generate next path segment.
+            Vector3 wi;
+            if (shading_ctx.specular_scattering.sample_delta_direction) {
+                wi = shading_ctx.specular_scattering.delta_direction;
+                path_coeff *= shading_ctx.specular_scattering.scattering_coeff;
+            }
+            else {
+                float bsdf_pdf;
+                ColorRGB f = shading_ctx.bsdf->sample(u_bsdf_next_segment, shading_ctx.wo, &wi, &bsdf_pdf);
+                if (f.is_black())
+                    break;
+                path_coeff *= f * (shading_ctx.specular_scattering.finite_scattering_weight * std::abs(dot(shading_ctx.normal, wi)) / bsdf_pdf);
+            }
+            current_ray.origin = shading_ctx.get_ray_origin_using_control_direction(wi);
+            current_ray.direction = wi;
         }
-        current_ray.origin = shading_ctx.get_ray_origin_using_control_direction(wi);
-        current_ray.direction = wi;
+        else if (shading_ctx.specular_scattering.type == Specular_Scattering_Type::specular_reflection) {
+            current_ray.direction = reflect(shading_ctx.wo, shading_ctx.normal);
+            current_ray.origin = shading_ctx.get_ray_origin_using_control_direction(current_ray.direction);
+            if (shading_ctx.has_dxdy_derivatives && path_ctx.bounce_count < max_differential_ray_bounces) {
+                specular_differential_rays = shading_ctx.compute_differential_rays_for_specular_reflection(current_ray);
+                use_specular_differential_rays = true;
+            }
+            path_coeff *= shading_ctx.specular_scattering.scattering_coeff;
+            path_ctx.perfect_specular_bounce_count++;
+            path_ctx.bounce_count++;
+        }
+        else if (shading_ctx.specular_scattering.type == Specular_Scattering_Type::specular_transmission) {
+            const float eta = shading_ctx.specular_scattering.etaI_over_etaT;
+            const bool refracted = refract(shading_ctx.wo, shading_ctx.normal, eta, &current_ray.direction);
+            ASSERT(refracted); // specular_transmission event should never be selected when total internal reflection happens
+            current_ray.origin = shading_ctx.get_ray_origin_using_control_direction(current_ray.direction);
+            if (shading_ctx.has_dxdy_derivatives && path_ctx.bounce_count < max_differential_ray_bounces) {
+                specular_differential_rays = shading_ctx.compute_differential_rays_for_specular_transmission(current_ray, eta);
+                use_specular_differential_rays = true;
+            }
+            path_coeff *= shading_ctx.specular_scattering.scattering_coeff;
+            path_ctx.perfect_specular_bounce_count++;
+            path_ctx.bounce_count++;
+        }
 
         // Apply russian roulette.
-        if (path_ctx.bounce_count >= bounce_count_when_to_apply_russian_roulette) {
-            // That's fine to get the next sample inside condition because condition
-            // is evaluated to the same value for all threads.
+        if (path_ctx.bounce_count >= rt_config.russian_roulette_bounce_count_threshold) {
+
+            // That's fine to get the next sample inside the above condition because that
+            // condition is evaluated to the same value for all paths at the given depth.
+            // The same reasoning explains why we can't move this call down even further
+            // and to have it inside the next condition - that condition is a function
+            // of the current path and the condition evaluation result is not the same among
+            // different paths at the given depth.
             float u_termination = thread_ctx.pixel_sampler.get_next_1d_sample();
 
             float max_coeff = std::max(path_coeff[0], std::max(path_coeff[1], path_coeff[2]));
-            float termination_probability = std::max(0.05f, 1.f - max_coeff);
 
-            if (u_termination < termination_probability)
-                break;
-
-            path_coeff /= 1 - termination_probability;
+            if (max_coeff < rt_config.russian_roulette_radiance_threshold) {
+                float termination_probability = std::max(0.05f, 1.f - max_coeff);
+                if (u_termination < termination_probability) {
+                    break;
+                }
+                path_coeff /= 1.f - termination_probability;
+            }
         }
     }
     return L;
