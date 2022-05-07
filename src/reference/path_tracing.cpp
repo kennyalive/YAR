f85b741345f20660476fcc5e047669b265aa20ee
Artem Kharytoniuk <artemalive@gmail.com>
2022-05-07 16:36:22 +0200

    ref: Fix issues with roulette activation for delta scattering event
    Delta scattering contribution was applied on the next loop iteration because
    we re-used the ray cast at the beginning of the next iteration and it resulted
    in a nice code with only a single ray cast for the entire algorithm (but not
    much of other benefits).
    
    Unfortunately it resulted in a probelm that russian roulette activated more
    early for the delta events comparing to bsdf events. Additionally, the roulette
    compensation coefficient 1/(1-q) was applied to the path before the light
    contribution is computed on the next loop iteration. Lighting should always
    be applied to the path value before it is adjusted by the roulette - the adjustment
    ensures unbiased results for the subsequent paths but not for the current one.
    
    The new code fixes the problem by handling delta scattering in the same way
    as bsdf scattering - all computations are performed in the same loop
    iteration.

--- src/reference/path_tracing.cpp
+++ src/reference/path_tracing.cpp
@@ -7,61 +7,49 @@
 #include "direct_lighting.h"
 #include "shading_context.h"
 
-#include "lib/scene.h"
-
 ColorRGB estimate_path_contribution(Thread_Context& thread_ctx, const Ray& ray, const Differential_Rays& differential_rays)
 {
     const Scene_Context& scene_ctx = *thread_ctx.scene_context;
     const Shading_Context& shading_ctx = thread_ctx.shading_context;
-    const Delta_Scattering& delta_scattering = shading_ctx.delta_scattering;
     const Raytracer_Config& rt_config = scene_ctx.raytracer_config;
 
     Path_Context& path_ctx = thread_ctx.path_context;
     Ray current_ray = ray;
     ColorRGB path_coeff = Color_White;
+    bool ray_is_already_traced_by_delta_bounce = false;
 
     ColorRGB L;
     while (true) {
+        if (!ray_is_already_traced_by_delta_bounce) {
+            bool hit_found = trace_ray(thread_ctx, current_ray, path_ctx.bounce_count == 0 ? &differential_rays : nullptr);
+
+            // Collect directly visible emitted light.
+            if (path_ctx.bounce_count == 0) {
+                if (hit_found)
+                    L += path_coeff * get_emitted_radiance(thread_ctx);
+                else if (scene_ctx.has_environment_light_sampler)
+                    L += path_coeff * scene_ctx.environment_light_sampler.get_filtered_radiance_for_direction(shading_ctx.miss_ray.direction);
+
+                if (rt_config.max_light_bounces == 0)
+                    break;
+            }
+
+            // About area light check: in current design we don't have scattering on area light
+            // sources (shading_ctx.bsdf == nullptr). Hitting area light ends path generation
+            // (subsequent segments have no effect due to zero bsdf on area light).
+            if (!hit_found || shading_ctx.area_light != Null_Light)
+                break;
+        }
+        ray_is_already_traced_by_delta_bounce = false;
+
         float u_init_scattering = thread_ctx.pixel_sampler.get_next_1d_sample();
         float u_light_index = thread_ctx.pixel_sampler.get_next_1d_sample();
         Vector2 u_light_mis = thread_ctx.pixel_sampler.get_next_2d_sample();
         Vector2 u_bsdf_mis = thread_ctx.pixel_sampler.get_next_2d_sample();
         Vector2 u_bsdf_next_segment = thread_ctx.pixel_sampler.get_next_2d_sample();
 
-        // Store delta scattering information from the last hit. The next trace ray will
-        // reset it to default values, but we still need it to be available after that point.
-        bool collect_emitted_radiance_after_delta_bounce = shading_ctx.delta_scattering_event;
-        Differential_Rays delta_differential_rays = delta_scattering.differential_rays;
-
-        // Check if we have differential rays available.
-        const Differential_Rays* p_differential_rays = nullptr;
-        if (path_ctx.bounce_count == 0)
-            p_differential_rays = &differential_rays;
-        else if (delta_scattering.has_differential_rays)
-            p_differential_rays = &delta_differential_rays;
-
-        bool hit_found = trace_ray(thread_ctx, current_ray, p_differential_rays);
-
-        // Special cases to collect emitted light.
-        if (path_ctx.bounce_count == 0 || collect_emitted_radiance_after_delta_bounce) {
-            if (hit_found)
-                L += path_coeff * get_emitted_radiance(thread_ctx);
-            else if (scene_ctx.has_environment_light_sampler)
-                L += path_coeff * scene_ctx.environment_light_sampler.get_filtered_radiance_for_direction(shading_ctx.miss_ray.direction);
-
-            if (path_ctx.bounce_count == rt_config.max_light_bounces)
-                break;
-        }
-
-        // About area light check: in current design we don't have scattering on area light
-        // sources (shading_ctx.bsdf == nullptr). Hitting area light ends path generation
-        // (subsequent segments have no effect due to zero bsdf on area light).
-        if (!hit_found || shading_ctx.area_light != Null_Light)
-            break;
-
         thread_ctx.shading_context.initialize_scattering(thread_ctx, u_init_scattering);
 
-        Vector3 wi;
         if (!shading_ctx.delta_scattering_event) {
             ColorRGB direct_lighting = estimate_direct_lighting_from_single_sample(thread_ctx, u_light_index, u_light_mis, u_bsdf_mis);
             L += path_coeff * direct_lighting;
@@ -70,26 +58,53 @@ ColorRGB estimate_path_contribution(Thread_Context& thread_ctx, const Ray& ray, 
             if (path_ctx.bounce_count == rt_config.max_light_bounces)
                 break;
 
+            Vector3 wi;
             float bsdf_pdf;
             ColorRGB f = shading_ctx.bsdf->sample(u_bsdf_next_segment, shading_ctx.wo, &wi, &bsdf_pdf);
             if (f.is_black())
                 break;
 
             path_coeff *= f * (std::abs(dot(shading_ctx.normal, wi)) / (bsdf_pdf * shading_ctx.bsdf_layer_selection_probability));
+
+            current_ray.origin = shading_ctx.get_ray_origin_using_control_direction(wi);
+            current_ray.direction = wi;
         }
         else {
             if (shading_ctx.bsdf) {
                 ColorRGB direct_lighting = estimate_direct_lighting_from_single_sample(thread_ctx, u_light_index, u_light_mis, u_bsdf_mis);
                 L += path_coeff * direct_lighting;
             }
-            wi = delta_scattering.delta_direction;
-            path_coeff *= delta_scattering.attenuation;
-            path_ctx.perfect_specular_bounce_count++;
-            path_ctx.bounce_count++;
-        }
 
-        current_ray.origin = shading_ctx.get_ray_origin_using_control_direction(wi);
-        current_ray.direction = wi;
+            Delta_Scattering ds = shading_ctx.delta_scattering;
+
+            Ray delta_ray;
+            delta_ray.origin = shading_ctx.get_ray_origin_using_control_direction(ds.delta_direction);
+            delta_ray.direction = ds.delta_direction;
+
+            bool delta_hit_found = trace_ray(thread_ctx, delta_ray,
+                ds.has_differential_rays ? &ds.differential_rays : nullptr);
+
+            ColorRGB emitted_radiance;
+            if (delta_hit_found)
+                emitted_radiance = get_emitted_radiance(thread_ctx);
+            else if (scene_ctx.has_environment_light_sampler)
+                emitted_radiance = scene_ctx.environment_light_sampler.get_filtered_radiance_for_direction(
+                    shading_ctx.miss_ray.direction);
+
+            path_coeff *= ds.attenuation;
+            L += path_coeff * emitted_radiance;
+
+            path_ctx.bounce_count++;
+            if (path_ctx.bounce_count == rt_config.max_light_bounces)
+                break;
+
+            path_ctx.perfect_specular_bounce_count++;
+
+            if (!delta_hit_found || shading_ctx.area_light != Null_Light)
+                break;
+
+            ray_is_already_traced_by_delta_bounce = true;
+        }
 
         // Apply russian roulette.
         if (path_ctx.bounce_count >= rt_config.russian_roulette_bounce_count_threshold) {
