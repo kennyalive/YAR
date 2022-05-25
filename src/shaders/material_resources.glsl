04f77534878fb6b47223201288cae78d38adc407
Artem Kharytoniuk <artemalive@gmail.com>
2022-05-25 19:54:58 +0200

    gpu: Fix hlsl material type mismatch with c++ declarations

--- src/shaders/material_resources.glsl
+++ src/shaders/material_resources.glsl
@@ -4,8 +4,8 @@
 #include "shared_main.h"
 #include "shared_material.h"
 
-const int Material_None = 0; 
-const int Material_Lambertian = 1;
+const int Material_Lambertian = 0;
+const int Material_None = -1; 
 
 layout(std430, set=MATERIAL_SET_INDEX, binding=LAMBERTIAN_MATERIAL_BINDING)
 buffer Lambertian_Material_Buffer {
